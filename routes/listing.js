6959be60c5c75b34f95c7c74c76632e7176efcdd
piyush0189 <piyushpushkar2004@gmail.com>
2025-04-08 16:26:14 +0530

    feat: implement flash messages for success notifications on listing and review actions

--- routes/listing.js
+++ routes/listing.js
@@ -54,6 +54,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id", validateListing,  wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -62,6 +63,7 @@ router.delete("/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
    let deletedListing = await Listing.findByIdAndDelete(id);
    console.log(deletedListing);
+   req.flash("success", "Listing Deleted!");
    res.redirect("/listings");
 }));
 
