3cda860dded33db9720361dd5582a36a0f0ebc61
piyush0189 <piyushpushkar2004@gmail.com>
2025-04-09 22:03:07 +0530

    feat: add authentication middleware and protect routes for listings

--- routes/user.js
+++ routes/user.js
@@ -38,4 +38,14 @@ router.post(
             res.redirect("/listings");
 });
 
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if(err) {
+           return next(err);
+        }
+        req.flash("success", "you are logged out!");
+        res.redirect("/listings");
+    });
+});
+
 module.exports = router;
