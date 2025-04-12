f1bb51db2f1a75e86ec1e4ea9ae766eb97c67ad0
piyush0189 <piyushpushkar2004@gmail.com>
2025-04-12 18:27:52 +0530

    feat: implement ownership validation for listings and enhance review validation

--- routes/listing.js
+++ routes/listing.js
@@ -2,19 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema} = require("../schema.js");
-const {isLoggedIn} = require("../middleware.js");
+const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 
-const validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-       throw new ExpressError(400, errMsg); 
-    }else{
-        next();
-    }
-};
+
 
 //Index Route
 router.get("/", wrapAsync(async (req, res) => {
@@ -51,7 +41,7 @@ router.post("/", isLoggedIn, validateListing,
 ));
 
 //Edit Route
-router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
+router.get("/:id/edit",isLoggedIn,isOwner, wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     if(!listing) {
@@ -62,7 +52,7 @@ router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-router.put("/:id", isLoggedIn, validateListing,  wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn,isOwner, validateListing,  wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     req.flash("success", "Listing Updated!");
@@ -70,7 +60,7 @@ router.put("/:id", isLoggedIn, validateListing,  wrapAsync(async (req, res) => {
 }));
 
 //Delete Route
-router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn,isOwner, wrapAsync(async (req, res) => {
     let {id} = req.params;
    let deletedListing = await Listing.findByIdAndDelete(id);
    console.log(deletedListing);
