0797e6d0041e44ba60149cdfffc3739dc01eb1b1
piyush0189 <piyushpushkar2004@gmail.com>
2025-04-14 18:54:49 +0530

    added multer

--- routes/listing.js
+++ routes/listing.js
@@ -4,14 +4,19 @@ const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require("multer")
+const upload = multer({dest: "uploads/"});
 
 
 router
    .route("/")
    .get(wrapAsync(listingController.index))
-   .post(isLoggedIn, validateListing, 
-      wrapAsync(listingController.createListing)
-   );
+   // .post(isLoggedIn, validateListing, 
+   //    wrapAsync(listingController.createListing)
+   // );
+   .post( upload.single("listing[image]"),(req, res) => {
+      res.send(req.file);
+   });
 
  //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
